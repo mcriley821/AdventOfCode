83e1ab453f4df6675a803156c8f962a00c01c5a5
Matthew <mcriley821@gmail.com>
2022-09-07 21:58:39 -0500

    2021 Puzzle 7 solution

--- Puzzles/2021/Puzzle07.py
+++ Puzzles/2021/Puzzle07.py
@@ -49,10 +49,55 @@
 #  Determine the horizontal position that the crabs can align to using the least
 #  fuel possible. How much fuel must they spend to align to that position?
 #
+#  --- Part Two ---
+#  The crabs don't seem interested in your proposed solution. Perhaps you
+#  misunderstand crab engineering?
+#  
+#  As it turns out, crab submarine engines don't burn fuel at a constant rate.
+#  Instead, each change of 1 step in horizontal position costs 1 more unit of
+#  fuel than the last: the first step costs 1, the second step costs 2, the
+#  third step costs 3, and so on.
+#  
+#  As each crab moves, moving further becomes more expensive. This changes the
+#  best horizontal position to align them all on; in the example above, this
+#  becomes 5:
+#  
+#  Move from 16 to 5: 66 fuel
+#  Move from 1 to 5: 10 fuel
+#  Move from 2 to 5: 6 fuel
+#  Move from 0 to 5: 15 fuel
+#  Move from 4 to 5: 1 fuel
+#  Move from 2 to 5: 6 fuel
+#  Move from 7 to 5: 3 fuel
+#  Move from 1 to 5: 10 fuel
+#  Move from 2 to 5: 6 fuel
+#  Move from 14 to 5: 45 fuel
+#
+#  This costs a total of 168 fuel. This is the new cheapest possible outcome;
+#  the old alignment position (2) now costs 206 fuel instead.
+#  
+#  Determine the horizontal position that the crabs can align to using the least
+#  fuel possible so they can make you an escape route! How much fuel must they
+#  spend to align to that position?
 from get_input import get_input
 
+
+def fuel_cost_v2(distance: int) -> int:
+    return ((distance + 1) * distance) // 2
 
 
 def main():
     given = get_input(7, 2021)
+    n = list(sorted(map(int, given.split(','))))
+    median = n[len(n) // 2]
+    print("Part 1:", sum(abs(median - i) for i in n))
+
+    mean = int(sum(n) / len(n))
+    means = range(mean - 5, mean + 5)
+    sums = (sum(map(fuel_cost_v2, (abs(m - i) for i in n))) for m in means)
+    print("Part 2:", min(sums))
+    
+
+if __name__ == "__main__":
+    main()
 
